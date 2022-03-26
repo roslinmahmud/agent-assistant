acd39e2acdc12910eddf7310080bc59cfe28af53
Roslin Mahmud Joy <90022016+roslin-bs23@users.noreply.github.com>
2022-03-26 11:46:14 +0600

    Migrated to .NET 6

--- AgentAssistant/Startup.cs
+++ AgentAssistant/Startup.cs
@@ -94,11 +94,7 @@ namespace AgentAssistant
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
+            if (!env.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
