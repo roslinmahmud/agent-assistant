f1950a85faa07bb4cd6cbef67b6e46f81117a489
Roslin Mahmud Joy <roslinmahmudjoy@gmail.com>
2022-05-20 18:02:35 +0600

    Removed html2canvas PDF generation

--- AgentAssistant/ClientApp/src/app/features/statement/statement-form/statement-form.component.ts
+++ AgentAssistant/ClientApp/src/app/features/statement/statement-form/statement-form.component.ts
@@ -43,20 +43,7 @@ export class StatementFormComponent implements OnInit {
   }
 
   public openPDF():void {
-    let DATA = document.getElementById('statementData');
-      
-    html2canvas(DATA).then(canvas => {
-        
-        let fileWidth = 208;
-        let fileHeight = canvas.height * fileWidth / canvas.width;
-        
-        const FILEURI = canvas.toDataURL('image/png')
-        let PDF = new jsPDF('p', 'mm', 'a4');
-        let position = 0;
-        PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight)
-        
-        PDF.save('angular-demo.pdf');
-    });     
+    print();
   }
 
   onSubmit(): void {
